e8c1b3d6cd06d6bde5de91f53af1344a35879683
harrisimon <harrisimon@gmail.com>
2023-08-09 11:14:47 -0400

    initial commit of vite app

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './App.css'
+
+
+
+function App() {
+
+
+  return (
+    <>
+    <div>
+      <h1>Find Me</h1>
+      <button>Find Me</button>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
