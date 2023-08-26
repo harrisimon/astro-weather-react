52218c25178d0f2fb40f4ca442977b564c67a20a
harrisimon <harrisimon@gmail.com>
2023-08-26 16:34:35 -0400

    adjuisted proxy in new structure:

--- netlify/functions/proxy/proxy.js
+++ netlify/functions/proxy/proxy.js
@@ -1,17 +1,25 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
+
+  const variable1 = event.queryStringParameters.variable1;
+  const variable2 = event.queryStringParameters.variable2;
+  console.log(variable1, variable2)
+const url = `http://www.7timer.info/bin/api.pl?lon=${variable1}&lat=${variable2}&product=astro&output=json`; // Replace with your API's HTTP URL
+
+try {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return {
+    statusCode: 200,
+    body: data,
+  };
+} catch (error) {
+  return {
+    statusCode: 500,
+    body: JSON.stringify({ error: 'Failed to fetch data' }),
+  };
+}
 }
 
 module.exports = { handler }
