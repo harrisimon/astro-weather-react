caabfa91899472d52919388d4a980c8b6c71ec64
harrisimon <harrisimon@gmail.com>
2023-08-26 16:16:27 -0400

    adjusted req body

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -3,8 +3,12 @@
 const fetch = require('node-fetch');
 
 exports.handler = async (event, context) => {
-    const variable1 = event.queryStringParameters.variable1;
-    const variable2 = event.queryStringParameters.variable2;
+    const requestBody = JSON.parse(event.body);
+    const variable1 = requestBody.variable1;
+    const variable2 = requestBody.variable2;
+    // const variable1 = event.queryStringParameters.variable1;
+    // const variable2 = event.queryStringParameters.variable2;
+    console.log(variable1, variable2)
   const url = `http://www.7timer.info/bin/api.pl?lon=${variable1}&lat=${variable2}&product=astro&output=json`; // Replace with your API's HTTP URL
 
   try {
@@ -13,7 +17,7 @@ exports.handler = async (event, context) => {
 
     return {
       statusCode: 200,
-      body: JSON.stringify(data),
+      body: data,
     };
   } catch (error) {
     return {
