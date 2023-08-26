b1b6e0ed18e734560f4aa49d6a6cba0bc11dafb4
harrisimon <harrisimon@gmail.com>
2023-08-26 16:23:49 -0400

    switched to import from require in proxy

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,13 +1,11 @@
 // netlify/functions/proxy.js
 
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 
 exports.handler = async (event, context) => {
-    const requestBody = JSON.parse(event.body);
-    const variable1 = requestBody.variable1;
-    const variable2 = requestBody.variable2;
-    // const variable1 = event.queryStringParameters.variable1;
-    // const variable2 = event.queryStringParameters.variable2;
+
+    const variable1 = event.queryStringParameters.variable1;
+    const variable2 = event.queryStringParameters.variable2;
     console.log(variable1, variable2)
   const url = `http://www.7timer.info/bin/api.pl?lon=${variable1}&lat=${variable2}&product=astro&output=json`; // Replace with your API's HTTP URL
 
